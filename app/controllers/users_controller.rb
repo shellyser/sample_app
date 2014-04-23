e01dbd5bc68e90a6cb87a98dc9cc98b345a4fff3
Shelly Seroussi <shellyser@gmail.com>
2014-04-23 10:03:15 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   before_filter :admin_user,     only: :destroy
   def show
   @user = User.find(params[:id])  
+  @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -47,12 +48,7 @@ class UsersController < ApplicationController
 
   private
 
-      def signed_in_user
-        unless signed_in?
-            store_location
-            redirect_to signin_url, notice: "Please sign in." unless signed_in?
-          end
-        end
+ 
 
           def correct_user
             @user = User.find(params[:id])
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,6 +47,19 @@ describe "Authentication" do
           describe "for non-signed-in users" do
             let(:user) { FactoryGirl.create(:user) }
 
+            describe "in the Microposts controller" do
+
+                    describe "submitting to the create action" do
+                      before { post microposts_path }
+                      specify { response.should redirect_to(signin_path) }
+                    end
+
+                    describe "submitting to the destroy action" do
+                      before { delete micropost_path(FactoryGirl.create(:micropost)) }
+                      specify { response.should redirect_to(signin_path) }
+                    end
+                  end
+
             describe "in the Users controller" do
 
               describe "visiting the edit page" do
@@ -75,11 +88,11 @@ describe "Authentication" do
                             before { visit users_path }
                             it { should have_selector('title', text: 'Sign in') }
                           end
-                        end
-                end
-            end
+                  end
+             end
+           end
           end
-end
+      end
 
                 
           describe "as wrong user" do
@@ -106,8 +119,8 @@ end
                       describe "submitting a DELETE request to the Users#destroy action" do
                         before { delete user_path(user) }
                         specify { response.should redirect_to(root_url) }
-              end
-        end
-    end
-end
+                    end
+                end
+            end
+      end
 end
